ffa87e0db56ea8e2c3048f7b12ae1807b759a662
anafl4v1a <159205417+anafl4v1a@users.noreply.github.com>
2024-02-21 15:24:46 -0400

    Create af.java

---
+++ af.java
@@ -0,0 +1,36 @@
+import java.awt.FlowLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+public class LabelFrame extends JFrame{
+	private JLabel label1;
+	private JLabel label2;
+	private JLabel lavel3;
+	
+	
+	
+	public LabelFrame(){
+		super( "Testing JLabel" );
+		setLayout( new FlowLayout() );
+		
+		label1 = new JLabel( );
+		label1.setToolTipText( "This is label1" );
+		add( label1 );
+		
+		Icon bug = new ImageIcon( getClass().getResource( "if.png") );
+		label2 = new JLabel( " INSTITUTO FEDERAL DE EDUCAÇÃO, CIÊNCIA E TECNOLOGIA DE MATO GROSSO",bug, SwingConstants.LEFT);
+		label2.setToolTipText( "This is label2" );
+		add( label2 );
+		
+		lavel3 = new JLabel();
+		lavel3.setText( "Label with  icon and text at  bottom" );
+		lavel3.setIcon( bug );
+		lavel3.setHorizontalTextPosition( SwingConstants.CENTER );
+		lavel3.setVerticalTextPosition (SwingConstants.BOTTOM );
+		lavel3.setToolTipText( "This is lavel3" );
+		
+	}
+}
